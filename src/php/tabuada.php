068524b4a527a44597799c41ed5b063761addedf
plxulo <pcfdaa@gmail.com>
2023-04-05 11:21:04 -0300

    tabuada

--- src/php/tabuada.php
+++ src/php/tabuada.php
@@ -5,11 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inline PHP?</title>
+    <style>
+        .verde {
+            background: green;
+        }
+        .amarelo {
+            background: yellow;
+        }
+    </style>
 </head>
 <body>
     <?php
         $nome = "Maicol do SENAI";
         $resultado = (2+1) * 5;
+        
         echo("
             Linha 1
             <br>
@@ -17,8 +26,15 @@
             <br>
             Meu nome é $nome
             <br>
-            O resultado foi: $resultado
-        ")
+            O resultado foi:
+            <br>
+        ");
+
+        for($x=1; $x<=10; $x++) {
+            echo("<div class='verde'>");
+            echo(5*$x);
+            echo("</div>");
+        }
     ?>
 </body>
 </html>
